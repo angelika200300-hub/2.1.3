ce9d00373b5dda4dee6e60db177c96aa0237c914
angelika200300-hub <angelika200300@mail.ru>
2026-02-02 23:03:07 +0300

    site

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-var popup = document.querySelectorAll('.modal, show');
-var openPopupButton = document.querySelector('.button-open, button-show');
-var closePopupButton = document.querySelector('.button-close, button-lost');
+var popup = document.querySelectorAll('.modal');
+var openPopupButton = document.querySelector('.button-open');
+var closePopupButton = document.querySelector('.button-close');
 
 openPopupButton.addEventListener('click', function () {
 
 for (var i = 0; i < popup.length; i++)  {
-  popup[i].classList.remove('modal', 'show');
-  openPopupButton.classList.add('modal', 'show');
-  closePopupButton.classList.remove('modal', 'show');
+  popup[i].classList.remove('modal');
+  openPopupButton.classList.add('modal');
+  closePopupButton.classList.remove('modal');
 }});
 
 closePopupButton.addEventListener('click', function () {
 
   for (var i = 0; i < popup.length; i++)  {
-  popup[i].classList.add('modal', 'show');
-  closePopupButton.classList.add('modal', 'show');
-  openPopupButton.classList.remove('modal', 'show');
+  popup[i].classList.add('modal');
+  closePopupButton.classList.add('modal');
+  openPopupButton.classList.remove('modal');
+}});
+
+
+var repair = document.querySelectorAll('.show');
+var repairopenPopupButton = document.querySelector('.button-show');
+var repairclosePopupButton = document.querySelector('.button-lost');
+
+repairopenPopupButton.addEventListener('click', function () {
+
+for (var i = 0; i < repair.length; i++)  {
+  repair[i].classList.remove('show');
+  repairopenPopupButton.classList.add('show');
+  repairclosePopupButton.classList.remove('show');
+}});
+
+repairclosePopupButton.addEventListener('click', function () {
+
+  for (var i = 0; i < repair.length; i++)  {
+  repair[i].classList.add('show');
+  repairclosePopupButton.classList.add('show');
+  repairopenPopupButton.classList.remove('show');
 }});
 
 
